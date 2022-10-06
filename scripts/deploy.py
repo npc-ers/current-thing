3160db3e65d02f8eac4c129983890e42a4f0a360
Gerrit Hall <gerrit@rezscore.com>
2022-10-06 10:14:19 -0700

    chore: Final deployment script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
 
-from brownie import CurrentThing, NPC, Indoctrinator, accounts, network
+from brownie import NPC, CurrentThing, Indoctrinator, Wei, accounts, network
+from brownie.network import priority_fee
+
+if network.show_active() in ["goerli"]:
+    priority_fee("2 gwei")
 
 
 def main():
-    if network.show_active() in ['rinkeby', 'goerli']:
-        deployer = accounts.load('husky')
+    if network.show_active() in ["rinkeby", "goerli"]:
+        deployer = accounts.load("husky")
+    elif network.show_active() == "mainnet":
+        deployer = accounts.load("minnow")
     else:
         deployer = accounts[0]
 
     publish_flag = False
 
-    thing = CurrentThing.deploy({'from': deployer}, publish_source=publish_flag)
+    # Deploy Token
+    thing = CurrentThing.deploy({"from": deployer}, publish_source=publish_flag)
 
-    nft = NPC.deploy( {'from': deployer}, publish_source=publish_flag)
+    # Deploy NFT
+    nft = NPC.deploy({"from": deployer}, publish_source=publish_flag)
     thing.admin_set_npc_addr(nft, {"from": deployer})
 
-    minter = Indoctrinator.deploy({'from': deployer}, publish_source=publish_flag)
-    minter.admin_set_nft_addr(nft, {'from': deployer})
-    minter.admin_set_token_addr(thing, {'from': deployer})
-    thing.admin_set_minter(minter, {'from': deployer})
-    nft.set_minter(minter, {'from': deployer})
-    
+    # Deploy Minter
+    minter = Indoctrinator.deploy({"from": deployer}, publish_source=publish_flag)
+    minter.admin_set_nft_addr(nft, {"from": deployer})
+    minter.admin_set_token_addr(thing, {"from": deployer})
+    thing.admin_set_minter(minter, {"from": deployer})
+    nft.set_minter(minter, {"from": deployer})
